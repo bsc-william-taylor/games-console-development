89034be501f0968e80606f68a5244e457e5da45b
William Taylor <wi11berto@yahoo.co.uk>
2016-09-22 22:54:29 +0100

    basic playground project added

--- playground/main.cpp
+++ playground/main.cpp
@@ -1,14 +1,25 @@
-#include <iostream>
-using namespace std;
 
-int main()
+#include <iostream>
+#include <vector>
+
+#define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+
+#include "../stb_image_write.h"
+#include "../stb_image.h"
+
+int main(int argc, char * argv[])
 {
-	cout << "Hello World" << endl;
-	cout << "Hello World Again" << endl;
+	std::cout << "Image Loading..." << std::endl;
 	
-	for(int i = 0; i < 5; i++) {
-		cout << i << endl;
-	}
+	int width, height, components;
+
+	unsigned char * data = stbi_load("../picture.bmp", &width, &height, &components, 0);
 	
-	return (0);
+	std::cout << "w:" << width << " h: " << height << std::endl;
+	std::cout << "writing new image duplicate.png" << std::endl;
+
+	stbi_write_png("../duplicate.png", width, height, components, data, 0); 	
+	stbi_image_free(data);
+	return 0;
 }
