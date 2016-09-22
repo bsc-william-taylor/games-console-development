c31ba22784ea40296bbb4010598a6c25d7f296f3
William Taylor <wi11berto@yahoo.co.uk>
2016-09-22 20:40:12 +0100

    adding current work

---
+++ playground/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello World" << endl;
+	cout << "Hello World Again" << endl;
+	
+	for(int i = 0; i < 5; i++) {
+		cout << i << endl;
+	}
+	
+	return (0);
+}
