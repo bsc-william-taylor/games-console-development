fb11027d51c968f9efecf7ccfb2d5043506d4958
William Taylor <wi11berto@yahoo.co.uk>
2016-09-29 09:30:59 +0100

    updating build script

--- playground/build.py
+++ playground/build.py
@@ -2,12 +2,14 @@
 import sys
 import os
 
-# Double check python version
-print('python version: ' + sys.version)
-# Output so we know the script is running
-print('building PPU exe...')
-os.system('ppu-g++ -o ppu/ppu ppu/*.cpp -lspe2')
-print('finished')
-print('building spu exe...')
-os.system('spu-g++ -o spu/spu spu/*.cpp')
-print('finished')
+length = len(sys.argv)
+
+build_ppu = length >= 2 and sys.argv[1] == 'y'
+build_spu = length >= 2 and sys.argv[2] == 'y'
+
+if build_ppu:
+    print('building PPU exe...')
+    os.system('ppu-g++ -o ppu/ppu ppu/*.cpp -lspe2')
+if build_spu:
+    print('building SPU exe...')
+    os.system('spu-g++ -o spu/spu spu/*.cpp')
