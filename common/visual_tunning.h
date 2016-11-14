b759d3ac460fa6ba20fa67125501430ef20ebf02
William Taylor <wi11berto@yahoo.co.uk>
2016-11-14 15:30:39 +0000

    updating training values to better suit ps3

--- common/visual_tunning.h
+++ common/visual_tunning.h
@@ -2,8 +2,8 @@
 #define __VISUAL_TUNNING__
 
 #define ACCEPTED_VALUE_FOR_EDGE 50
-#define IGNORABLE_EDGE_DENSITY 0.3f
-#define PIXELS_PER_STEP 12
+#define IGNORABLE_EDGE_DENSITY 0.275
+#define PIXELS_PER_STEP 15
 #define REGION_LENGTH 45
 // these two used in case we do not want a square window
 #define REGION_HEIGHT 45
